17ccf0ce945ca2faec513ad46ec03abb4d56c3d4
John Burgoyne <john.holbrook.burgoyne@gmail.com>
2023-11-08 11:35:32 -0700

    Initial commit

---
+++ Models/Filters.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+
+namespace ToDo.Models
+{
+    public class Filters
+    {
+        public Filters(string filterstring)
+        {
+            filterstring = filterstring ?? "all-all-all";
+            string[] filters = filterstring.Split('-');
+            CategoryId = filters[0];
+            Due = filters[1];
+            StatusId = "all";
+        }
+
+        public string FilterString { get; }
+        public string CategoryId { get; }
+        public string Due { get; } 
+        public string StatusId { get; }
+        public bool HasCategory => CategoryId.ToLower() != "all";
+        public bool HasDue => Due.ToLower() != "all";
+        public bool HasStatus => StatusId.ToLower() != "all";
+        public static Dictionary<string, string> DueFilterValues =>
+            new Dictionary<string, string>
+            {
+                {"future","Future" },
+                {"past", "Past"},
+                {"today", "Today" }
+            };
+        public bool IsPast => Due.ToLower() == "past";
+        public bool IsFuture => Due.ToLower() == "future";
+        public bool IsToday => Due.ToLower() == "today";
+    }
+}
---
+++ Models/ToDoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+        public DbSet<ToDos> ToDos { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Status> Statuses { get; set; } = null!;
+
+        //seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = "work", Name = "Work"},
+                new Category { CategoryId = "home", Name = "Name"},
+                new Category { CategoryId = "ex", Name = "Exercise"},
+                new Category { CategoryId = "shop", Name = "Shop"},
+                new Category { CategoryId = "call", Name = "Call"}
+                );
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId="open", Name="Open"},
+                new Status { StatusId = "closed", Name = "Completed"}
+                );
+        }
+    }
+}
